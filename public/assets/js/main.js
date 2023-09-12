9ac965ac48c1df2561f60f231a369827d84d52e6
garcia-laura <jhony@Air-de-Jhony.home>
2023-09-12 14:33:14 +0200

    new style

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -2,4 +2,48 @@
 
 console.log('>> Ready :)');
 
+
+const emojis_setTech = ["💻", "🖥️", "📱", "⌨️", "💾", "🖱️", "🕹️", "🔌", "⌨️"];
+
+
+function randomEmoji() {
+	// change the emoji array here and it will reflect
+	return emojis_setTech[Math.floor(Math.random() * emojis_setTech.length)];
+}
+
+function createGrid() {
+	const container = document.getElementById("grid-container");
+
+	for (let i = 0; i < 100; i++) {
+		// Reduced the grid to 10x10
+		const emojiDiv = document.createElement("div");
+		emojiDiv.className = "emoji";
+		emojiDiv.textContent = randomEmoji();
+		const delay = Math.random() * 2;
+
+		// Apply the animation to only 50% of the emojis
+		if (Math.random() > 0.5) {
+			emojiDiv.classList.add("show-emoji");
+		}
+
+		emojiDiv.style.animationDelay = `${delay}s`;
+		container.appendChild(emojiDiv);
+	}
+}
+
+function updateGrid() {
+	const emojisDivs = document.querySelectorAll(".emoji");
+	emojisDivs.forEach((emojiDiv) => {
+		if (emojiDiv.classList.contains("show-emoji")) {
+			emojiDiv.textContent = randomEmoji();
+		}
+	});
+}
+
+function start() {
+	alert("Starting the journey of education!");
+}
+
+createGrid();
+setInterval(updateGrid, 10000);
 //# sourceMappingURL=main.js.map

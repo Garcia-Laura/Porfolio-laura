62fe242fe48d5912a107a36eef7116bb6f1c0b46
garcia-laura <jhony@Air-de-Jhony.home>
2023-09-21 10:44:54 +0200

    add new projects

--- src/js/main.js
+++ src/js/main.js
@@ -48,19 +48,66 @@ function start() {
 createGrid();
 setInterval(updateGrid, 10000);
 
- function handleClick(ev){
- 	ev.preventDefault();
- 	
-	console.log("hola dos")
+// //  function handleClick(ev){
+// //  	ev.preventDefault();
+// // 	console.log("hola dos")
+// //  }
+
+// //  function addEventIcon(){
+// // 	const menus = document.querySelectorAll(".js_menu");
+// //  	for (const eachMenus of menus){
+// //  		eachMenus.addEventListener("click", handleClick);
+// //  	}
+// // 	 console.log("click");
+// }
+
+const navButtons = document.querySelectorAll(".menu-nav ul li a");
+let anchors =[
+	document.querySelector('#sobreMi'),
+	document.querySelector('#proyectos'),
+	document.querySelector('#contacto'),
+	
+]
+let anchorYPosition = new Array();
+
+ function removeActive () {
+	for (let button of navButtons){
+		button.classList.remove('active')
+	}
+ } 
+ function toggleActiveAnchor () {
+	removeActive();
+	this.classList.add('active');
  }
 
- function addEventIcon(){
-	const menus = document.querySelectorAll(".js_menu");
- 	for (const eachMenus of menus){
- 		eachMenus.addEventListener("click", handleClick);
- 	}
-	 console.log("click");
-}
+ function updateAnchorYPosition(){
+	for (var i = 0; i < anchors.length; i++){
+		anchorYPosition[i] = anchors[i].getBoundingClientRect().top;
+	}
+ }
+
+ for (var i = 0; i < navButtons.length; i++){
+	navButtons[i].addEventListener("click",toggleActiveAnchor )
+ }
+
+ anchorYPosition.forEach(element =>{
+	console.log(element)
+ });
+
+ let currentAnchorIndex = null;
+
+ window.onscroll = () =>{
+	const offSet = window.innerHeight * 0.6;
+	updateAnchorYPosition();
+	for (let i = 0; i < anchorYPosition.length; i++){
+		if(i != currentAnchorIndex && anchorYPosition[i] - offSet <= 0 && (i === 4) || anchorYPosition[i + 1] > 0)
+	{
+		currentAnchorIndex = i;
+		removeActive();
+		navButtons[i].classList.add('active');
+	}
+	}
+ }
 
 
 // function handleClick (){
